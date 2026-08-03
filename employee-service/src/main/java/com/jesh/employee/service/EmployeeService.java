a2c1adc73f2ae70eea5b6184b0621513a61e03e3
Jeshwanth B.L <jeshwanth1493@gmail.com>
2026-08-04 01:42:25 +0530

    Added Employee Service CI/CD pipeline

--- employee-service/src/main/java/com/jesh/employee/service/EmployeeService.java
+++ employee-service/src/main/java/com/jesh/employee/service/EmployeeService.java
@@ -18,7 +18,7 @@ public class EmployeeService {
 
         employeeMap.put(1L, Employee.builder()
                 .id(1L)
-                .firstName("Jeshwanth1")
+                .firstName("Jeshwanth1423")
                 .lastName("Lokesh")
                 .email("jeshwanth@example.com")
                 .department("Engineering")
